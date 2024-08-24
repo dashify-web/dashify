b5b1c39168689f324e8a7df787eaca6404554f80
RickaPrincy <rckprincy@gmail.com>
2024-08-24 19:17:18 +0300

    chore: first attemps of eslint & prettier config

--- packages/providers/__tests__/providers.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const providers = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(providers(), 'Hello from providers');
-console.info('providers tests passed');
